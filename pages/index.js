8d6d57a89308537d28ec9982c68b3746884f05d4
Le Minh Giang <mgiang2015@gmail.com>
2021-06-17 00:29:39 +0800

    Close html tag

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,8 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Wassup people!<p>
+        <p>Wassup people!</p>
+        <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
